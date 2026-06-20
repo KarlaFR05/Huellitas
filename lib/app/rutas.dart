50f88e14e3287109e7e4c897336becf3abee7d60
MarleneBeris05 <mbruiz.2005@gmail.com>
2026-06-20 00:48:23 -0600

    Actualización de rutas y main

--- lib/app/rutas.dart
+++ lib/app/rutas.dart
@@ -1,27 +1,40 @@
 import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
-import '../componentes/splash/splash_screen.dart';
-import '../componentes/welcome/welcome_screen.dart';
-import '../componentes/auth/login_screen.dart';
-import '../componentes/auth/registro_screen.dart';
-import '../componentes/auth/password_screen.dart';
-import '../componentes/home/home_screen.dart';
+import 'package:huellitas/features/splash/presentation/screens/splash_screen.dart';
+import 'package:huellitas/features/welcome/presentation/screens/welcome_screen.dart';
+import 'package:huellitas/features/auth/presentation/screens/login_screen.dart';
+import 'package:huellitas/features/auth/presentation/screens/registro_screen.dart';
+import 'package:huellitas/features/auth/presentation/screens/password_screen.dart';
+import 'package:huellitas/features/home/presentation/screens/home_screen.dart';
+
+import 'package:huellitas/features/reporte/data/datasources/reporte_remote_datasource_impl.dart';
+import 'package:huellitas/features/reporte/data/repositories/reporte_repository_impl.dart';
+import 'package:huellitas/features/reporte/domain/usecases/create_reporte_usecase.dart';
+
+import 'package:huellitas/features/reporte/presentation/bloc/reporte_event.dart';
+import 'package:huellitas/features/reporte/data/datasources/catalog_remote_datasource_impl.dart';
+import 'package:huellitas/features/reporte/data/repositories/catalog_repository_impl.dart';
+import 'package:huellitas/features/reporte/domain/usecases/get_animal_types.dart';
+import 'package:huellitas/features/reporte/domain/usecases/get_report_types.dart';
+import 'package:huellitas/features/reporte/domain/usecases/get_urgency_levels.dart';
+
+import 'package:huellitas/features/reporte/presentation/bloc/reporte_bloc.dart';
+import 'package:huellitas/features/reporte/presentation/report_form_screen.dart';
+
+import 'package:huellitas/features/reporte/presentation/report_success_screen.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const SplashScreen(),
-    ),
+    GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
     GoRoute(
       path: '/welcome',
       builder: (context, state) => const WelcomeScreen(),
     ),
-    GoRoute(
-      path: '/login',
-      builder: (context, state) => const LoginScreen(),
-    ),
+    GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
     GoRoute(
       path: '/register',
       builder: (context, state) => const RegistroScreen(),
@@ -30,9 +43,43 @@ final GoRouter router = GoRouter(
       path: '/password',
       builder: (context, state) => const PasswordScreen(),
     ),
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
     GoRoute(
-      path: '/home',
-      builder: (context, state) => const HomeScreen(),
+      path: '/report-success',
+      builder: (context, state) => const ReportSuccessScreen(),
+    ),
+
+    GoRoute(
+      path: '/report-form',
+      pageBuilder: (context, state) {
+        final dio = Dio(
+          BaseOptions(
+            baseUrl: 'http://192.168.1.68:8000',
+            connectTimeout: const Duration(seconds: 7),
+            receiveTimeout: const Duration(seconds: 5),
+          ),
+        );
+
+        final reporteRepository = ReporteRepositoryImpl(
+          ReporteRemoteDataSourceImpl(dio),
+        );
+
+        final catalogRepository = CatalogRepositoryImpl(
+          CatalogRemoteDataSourceImpl(dio),
+        );
+
+        return MaterialPage(
+          child: BlocProvider(
+            create: (_) => ReporteBloc(
+              crearReporte: CreateReporteUseCase(reporteRepository),
+              getAnimalTypes: GetAnimalTypes(catalogRepository),
+              getReportTypes: GetReportTypes(catalogRepository),
+              getUrgencyLevels: GetUrgencyLevels(catalogRepository),
+            ),
+            child: const ReportFormScreen(),
+          ),
+        );
+      },
     ),
   ],
 );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'features/auth/presentation/bloc/auth_bloc.dart';
 
 void main() {
-  runApp(const HuellitasApp());
+  runApp(
+    BlocProvider(
+      create: (_) => AuthBloc(),
+      child: const HuellitasApp(),
+    ),
+  );
 }
