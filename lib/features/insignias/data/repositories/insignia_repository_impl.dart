e62035647121ec08e68a5e2911e30c049ca270e1
AndryCarmona <andrycarmona123@gmail.com>
2026-07-08 22:26:44 -0600

    Merge de todo sin insignias conectadas al backend

--- lib/features/insignias/data/repositories/insignia_repository_impl.dart
+++ lib/features/insignias/data/repositories/insignia_repository_impl.dart
@@ -1,7 +1,7 @@
 import '../../domain/entities/insignia.dart';
 import '../../domain/entities/categoria_insignia.dart';
 import '../../domain/repositories/insignia_repository.dart';
-import '../datasources/insignia_remote_datasource.dart';
+import '../datasources/insignia_remote_datasource_impl.dart';
 
 class InsigniaRepositoryImpl implements InsigniaRepository {
   final InsigniaRemoteDataSource remoteDataSource;
@@ -9,13 +9,21 @@ class InsigniaRepositoryImpl implements InsigniaRepository {
   InsigniaRepositoryImpl(this.remoteDataSource);
 
   @override
-  Future<Map<CategoriaInsignia, List<Insignia>>> obtenerTodasLasInsignias(int usuarioId) async {
+  Future<Map<CategoriaInsignia, List<Insignia>>> obtenerTodasLasInsignias(
+    int usuarioId,
+  ) async {
     final insignias = await remoteDataSource.obtenerInsignias(usuarioId);
-    
+
     return {
-      CategoriaInsignia.rescate: insignias.where((i) => i.categoria == CategoriaInsignia.rescate).toList(),
-      CategoriaInsignia.donacion: insignias.where((i) => i.categoria == CategoriaInsignia.donacion).toList(),
-      CategoriaInsignia.reporte: insignias.where((i) => i.categoria == CategoriaInsignia.reporte).toList(),
+      CategoriaInsignia.rescate: insignias
+          .where((i) => i.categoria == CategoriaInsignia.rescate)
+          .toList(),
+      CategoriaInsignia.donacion: insignias
+          .where((i) => i.categoria == CategoriaInsignia.donacion)
+          .toList(),
+      CategoriaInsignia.reporte: insignias
+          .where((i) => i.categoria == CategoriaInsignia.reporte)
+          .toList(),
     };
   }
 }
