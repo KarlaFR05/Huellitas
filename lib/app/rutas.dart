4aa22194e1a3cdcf5c9a5b266ffea33d81b89af0
KarlaFR05 <karladfr.15@gmail.com>
2026-07-08 21:16:32 -0600

    Insignias

--- lib/app/rutas.dart
+++ lib/app/rutas.dart
@@ -141,7 +141,7 @@ final GoRouter router = GoRouter(
       pageBuilder: (context, state) {
         final dio = Dio(
           BaseOptions(
-            baseUrl: 'http://192.168.100.83:8000',
+            baseUrl: 'https://huellitas-backend-xekn.onrender.com',
             connectTimeout: const Duration(seconds: 30),
             receiveTimeout: const Duration(seconds: 30),
             sendTimeout: const Duration(seconds: 30),
@@ -175,7 +175,7 @@ final GoRouter router = GoRouter(
       pageBuilder: (context, state) {
         final dio = Dio(
           BaseOptions(
-            baseUrl: 'http://192.168.100.83:8000',
+            baseUrl: 'https://huellitas-backend-xekn.onrender.com',
             connectTimeout: const Duration(seconds: 30),
             receiveTimeout: const Duration(seconds: 30),
           ),
