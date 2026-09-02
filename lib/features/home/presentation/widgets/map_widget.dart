c9939ccbf7cee769d8cfe609ed6c272e4b52ed58
AndryCarmona <andrycarmona123@gmail.com>
2026-09-02 07:11:35 -0600

    maldito mapa

--- lib/features/home/presentation/widgets/map_widget.dart
+++ lib/features/home/presentation/widgets/map_widget.dart
@@ -167,7 +167,7 @@ class _MapWidgetState extends State<MapWidget> {
           ),
           child: TileLayer(
             urlTemplate:
-                'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+                'https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png?key=cb1_2b6j_1_8eab03dbde9219c848b05fa1',
             subdomains: const ['a', 'b', 'c', 'd'],
             userAgentPackageName: 'com.huellitas.app',
           ),
